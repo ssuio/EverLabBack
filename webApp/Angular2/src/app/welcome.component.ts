438cb205021b71717ac0d7ca1a721a0e85719af4
Ssuio <xssuio@gmail.com>
2017-02-20 22:03:52 +0800

    Add welcome.component.

---
+++ webApp/Angular2/src/app/welcome.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'welcome-everlab',
+  templateUrl: './welcome.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class WelcomeComponent {
+  title = 'welcome page';
+}
